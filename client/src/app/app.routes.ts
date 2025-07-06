3e413e5cc7713ab913e64d3edcf4083aacf3d115
Tayte555 <tayte2004@gmail.com>
2025-07-06 16:25:59 +0100

    setup tailwind frontend and routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterComponent } from './views/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '/login' },
+];
